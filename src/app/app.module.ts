570ca2cab8d3b041900cbcaeb05268c6dbaf7088
Braine Quidasol <brainequidasol@gmail.com>
2020-11-03 10:19:23 +0800

    11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserListComponent } from './private-pages/user-list/user-list.component';
+import { DashboardComponent } from './private-pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserListComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
